9aad64c5a4be2a75ce2ff8b3641974d2362352d6
KyronEpperson <77645855+KyronEpperson@users.noreply.github.com>
2021-12-09 16:58:45 -0600

    Add files via upload
    Thank you to 
    Jiaaro Github below
    https://github.com/jiaaro/pydub
    and
    CodingNepal - youtube.com/codingnepal

---
+++ music-list.js
@@ -0,0 +1,110 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "Concentrate",
+      artist: "Slimey Blimey",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Kalimba",
+      artist: "Kaught up",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Keys in the house",
+      artist: "Blueberry Bliss",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "SKYWALKIN",
+      artist: "Kango",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+        name: "Flavor",
+        artist: "Life",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+      name: "hazedemo",
+      artist: "SFA",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "paint",
+      artist: "SFA",
+      img: "music-7",
+      src: "music-7"
+    },
+    {
+      name: "Internet",
+      artist: "Life",
+      img: "music-8",
+      src: "music-8"
+    },
+    {
+      name: "rnb",
+      artist: "Life",
+      img: "music-9",
+      src: "music-9"
+  },
+  {
+    name: "sheesh",
+    artist: "Life",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "shop",
+    artist: "SFA",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "sometypeofway",
+    artist: "SFA",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "StuFlow",
+    artist: "LIfe",
+    img: "music-13",
+    src: "music-13"
+},
+{
+  name: "The Return",
+  artist: "Da Clout",
+  img: "music-14",
+  src: "music-14"
+},
+{
+  name: "untitled",
+  artist: "Life",
+  img: "music-15",
+  src: "music-15"
+},
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
